75d6d0aa7107cf83e1154bb4f417a022131ed34d
SeoHaebom <81906396+SeoHaebom@users.noreply.github.com>
2021-04-12 17:30:26 +0700

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -4,19 +4,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/home.html')
-})
-
-router.get('/api', (req, res) => {
     res.sendFile(__path + '/views/index.html')
 })
 
-router.get('/api/about', (req, res) => {
+router.get('/about', (req, res) => {
     res.sendFile(__path + '/views/about.html')
-})
-
-router.get('/api/news', (req, res) => {
-    res.redirect('https://raw.githubusercontent.com/Zhirrr/My-SQL-Results/main/Berita.json')
 })
 
 router.get('/config', (req, res) => {
@@ -24,9 +16,9 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
+            namabot: 'SachikoBot',
+            namaowner: 'hebom',
+            instagram: 'itsme.haebom',
             youtube : 'Gak Punya'
         }
     }
